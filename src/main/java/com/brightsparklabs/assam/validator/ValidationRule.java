5648e355ecafd79b0d12a642aa330fbe5f04ebd3
Al Jeyapal <al.jeyapal@brightsparklabs.com>
2015-08-14 14:47:46 +1000

    INC-7: Move ValidationRule interface to Assam

---
+++ src/main/java/com/brightsparklabs/assam/validator/ValidationRule.java
@@ -0,0 +1,73 @@
+/*
+ * Created by brightSPARK Labs
+ * www.brightsparklabs.com
+ */
+package com.brightsparklabs.assam.validator;
+
+import com.brightsparklabs.assam.data.AsnData;
+import com.google.common.collect.ImmutableSet;
+
+/**
+ * Represents a rule to validate {@link AsnData} against.
+ *
+ * @author brightSPARK Labs
+ */
+public interface ValidationRule
+{
+    // -------------------------------------------------------------------------
+    // CONSTANTS
+    // -------------------------------------------------------------------------
+
+    /** null instance */
+    ValidationRule.Null NULL = new ValidationRule.Null();
+
+    // -------------------------------------------------------------------------
+    // PUBLIC METHODS
+    // -------------------------------------------------------------------------
+
+    /**
+     * Validates the specified tag in the data
+     *
+     * @param tag
+     *         tag to validate in the data
+     * @param asnData
+     *         data to validate
+     *
+     * @return the results of validation
+     */
+    ImmutableSet<ValidationFailure> validate(String tag, AsnData asnData);
+
+    // -------------------------------------------------------------------------
+    // INTERNAL CLASS: NULL
+    // -------------------------------------------------------------------------
+
+    /**
+     * NULL instance of {@link ValidationRule}
+     *
+     * @author brightSPARK Labs
+     */
+    class Null implements ValidationRule
+    {
+        // ---------------------------------------------------------------------
+        // CONSTRUCTION
+        // ---------------------------------------------------------------------
+
+        /**
+         * Default constructor. This is private, use {@link ValidationRule#NULL} to obtain an
+         * instance
+         */
+        private Null()
+        {
+        }
+
+        // ---------------------------------------------------------------------
+        // IMPLEMENTATION: ValidationRule
+        // ---------------------------------------------------------------------
+
+        @Override
+        public ImmutableSet<ValidationFailure> validate(String tag, AsnData asnData)
+        {
+            return ImmutableSet.of();
+        }
+    }
+}
